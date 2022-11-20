e85b347701c2c7d46b8b70bd023f0f9243fecb95
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-20 16:56:58 -0300

    Aplicando estilos no Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content } from './styles';
 
 export function Header() {
     return (
-        <header>
-            <img src={logoImg} alt="np money" />
-            <button type="button">
-                Nova transação
-            </button>
-            
-        </header>
+        <Container>
+            <Content>
+                <img src={logoImg} alt="np money" />
+                <button type="button">
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
     )
 }
