c720cf2abdfaf66291b207c88a2c31b42358ab60
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-24 21:00:51 -0300

    Componente: NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
-import { Dashboard } from "./components/Dashboard";
+
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -25,12 +27,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
 
       <GlobalStyle />
     
