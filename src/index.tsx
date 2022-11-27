df4531a323e1e7bddc2b7c28817bb1383746a8cc
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-27 19:49:27 -0300

    Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,32 @@ import { App } from './App';
 
 createServer({
 
-    models: {
-      transaction: Model,
-    },
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date('2022-11-26 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 2000,
+          createdAt: new Date('2022-11-05 13:00:00'),
+        },
+      ]
+    })
+  },
 
   routes() {
     this.namespace = 'api';
